96be3d7bcd0d9330aad7ea1a72f7131225a2f53b
Usievaład <UsevaladK@playtika.by>
2015-12-22 12:48:42 +0300

    SmoothProgressBar of ArticleDialog became own ProgressBar.

--- main/src/main/java/org/anibyl/slounik/dialogs/ArticleDialog.java
+++ main/src/main/java/org/anibyl/slounik/dialogs/ArticleDialog.java
@@ -7,16 +7,16 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 import org.anibyl.slounik.Article;
 import org.anibyl.slounik.R;
 import org.anibyl.slounik.network.ArticlesCallback;
 import org.anibyl.slounik.network.ArticlesInfo;
 import org.anibyl.slounik.network.SlounikOrg;
+import org.anibyl.slounik.ui.ProgressBar;
 
 /**
  * Dialog for the text of an article.
- * <p>
+ * <p/>
  * Created by Usievaład Čorny on 01.03.2015 10:54.
  */
 public class ArticleDialog extends AlertDialog {
@@ -50,15 +50,13 @@ public class ArticleDialog extends AlertDialog {
 
         if (isLoadable) {
             final Button loadButton = (Button) findViewById(R.id.load);
-            final SmoothProgressBar progressBar = (SmoothProgressBar) findViewById(R.id.article_progress);
-            progressBar.setVisibility(View.INVISIBLE);
+            final ProgressBar progressBar = (ProgressBar) findViewById(R.id.article_progress);
             loadButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     loadButton.setEnabled(false);
 
                     if (article.getFullDescription() == null) {
-                        progressBar.setVisibility(View.VISIBLE);
                         progressBar.progressiveStart();
                         SlounikOrg.loadArticleDescription(article, context, new ArticlesCallback() {
                             @Override
