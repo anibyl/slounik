49827530eab64f88545b561ab500c5fffc2ad93b
Usievaład Kimajeŭ <usievalad@gmail.com>
2018-07-02 17:05:57 +0300

    Html: removing links from description text.

--- main/src/main/java/org/anibyl/slounik/core/Html.kt
+++ main/src/main/java/org/anibyl/slounik/core/Html.kt
@@ -4,16 +4,27 @@ import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.N
 import android.text.Html
 import android.text.Spanned
+import android.text.SpannedString
 
 /**
  * @author Usievaład Kimajeŭ
  * @created 05.05.2017
  */
 
+private val htmlLinkRegex = "</?a[^>]*>".toRegex()
+
 fun String?.fromHtml(): Spanned {
+	if (this == null) {
+		return SpannedString("")
+	}
+
+	var result: String = this
+
+	result = result.replace(htmlLinkRegex, "")
+
 	return if (SDK_INT < N) {
-		Html.fromHtml(this)
+		Html.fromHtml(result)
 	} else {
-		Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+		Html.fromHtml(result, Html.FROM_HTML_MODE_LEGACY)
 	}
 }
